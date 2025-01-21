0f243ffa588911a832bb613ebb0c0849e9d78231
schmidln <schmidln@bc.edu>
2025-01-21 12:18:26 -0500

    Initial Commit

---
+++ Ch 1.7 YouAreAwesome/Ch_1_7_YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ch_1_7_YouAreAwesomeApp.swift
+//  Ch 1.7 YouAreAwesome
+//
+//  Created by Lucas Schmidt on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Ch_1_7_YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ch 1.7 YouAreAwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ch 1.7 YouAreAwesome
+//
+//  Created by Lucas Schmidt on 1/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
